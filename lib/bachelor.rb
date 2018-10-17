faf97805e60d4462ca6dab6894eea77673e5148a
Jason Crescenzo <crescenzojay@gmail.com>
2018-10-17 18:02:50 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,10 @@
 def get_first_name_of_season_winner(data, season)
  winner = nil 
  puts "#{data}" 
- data.collect do
+ data.collect do |seasons, info|
+   if seasons == season.to_sym
+     info.collect do |contestant|
+       if 
 end
 
 def get_contestant_name(data, occupation)
