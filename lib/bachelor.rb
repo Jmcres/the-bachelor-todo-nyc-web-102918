4b16596e1eec6b12f733c9d99a849d0d05594929
Jason Crescenzo <crescenzojay@gmail.com>
2018-10-17 18:04:48 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,16 +1,9 @@
-def get_first_name_of_season_winner(data, season)
- winner = nil 
- puts "#{data}" 
- data.collect do |seasons, info|
-   if seasons == season.to_sym
-     info.collect do |contestant|
-       if contestant[:status] == "Winner"
-          winner = contestant[:name].split[0]
-        end 
-      end 
-    end 
-  end 
-winner
+def get_first_name_of_season_winner(data,season)
+ data[season.to_sym].each do |into_array|
+   if into_array[:status] =="Winner"
+     return into_array[:name].split(" ")[0]
+   end
+ end
 end
 
 def get_contestant_name(data, occupation)
