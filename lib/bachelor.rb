b02b9af2ecb5e2a253c0273b8865018967e76ab4
Jason Crescenzo <crescenzojay@gmail.com>
2018-10-17 18:03:00 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -4,7 +4,13 @@ def get_first_name_of_season_winner(data, season)
  data.collect do |seasons, info|
    if seasons == season.to_sym
      info.collect do |contestant|
-       if 
+       if contestant[:status] == "Winner"
+          winner = contestant[:name].split[0]
+        end 
+      end 
+    end 
+  end 
+winner
 end
 
 def get_contestant_name(data, occupation)
